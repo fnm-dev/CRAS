284035dba69c757405b52230808048031ca3a9db
Felipe Machado <f_nmachado@hotmail.com>
2025-12-20 20:28:42 -0300

    feat: Adicionado Logger

---
+++ CrasAPI/Common/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog.Events;
+
+namespace CrasAPI.Common
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected void Log(
+            LogEventLevel level,
+            string message,
+            Exception? ex = null,
+            params object[] args
+        )
+        {
+            var logger = Serilog.Log
+                .ForContext("Controller", GetType().Name)
+                .ForContext("Action", ControllerContext.ActionDescriptor.ActionName);
+
+            if (ex != null)
+                logger.Write(level, ex, message, args);
+            else
+                logger.Write(level, message, args);
+        }
+    }
+}
--- CrasAPI/Controllers/AuthController.cs
+++ CrasAPI/Controllers/AuthController.cs
@@ -1,14 +1,17 @@
-﻿using CrasAPI.DTO;
+﻿using CrasAPI.Common;
+using CrasAPI.DTO;
 using CrasAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Serilog.Events;
 
 namespace CrasAPI.Controllers
 {
     [ApiController]
     [Route("/auth")]
-    public class AuthController : ControllerBase
+    public class AuthController : BaseController
     {
         private readonly IAuthService _service;
+
         public AuthController(IAuthService service)
         {
             _service = service;
@@ -17,9 +20,11 @@ namespace CrasAPI.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginRequestDto dto)
         {
+            Log(LogEventLevel.Information, "Validating login of user {User}", null, dto.Username);
             var user = await _service.AuthenticateAsync(dto.Username, dto.Password);
             if (user == null)
             {
+                Log(LogEventLevel.Warning, "Login failed for user {User}", null, dto.Username);
                 return Unauthorized(new { message = "Invalid username or password" });
             }
             return Ok(new { message = "Login successful", userId = user.Id });
